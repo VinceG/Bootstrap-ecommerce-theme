b89c555fa0410f1c917064daf010d06dfaad55bc
Vincent <vadimg88@gmail.com>
2014-08-04 10:59:00 -0700

    added new pages

--- js/app.js
+++ js/app.js
@@ -4,4 +4,22 @@ $(function() {
 		var $imageLink = $(this).find('img').data('src');
 		$('.main-image').find('img').attr('src', $imageLink);
 	});
+
+	// Add new Product
+	$('.add-new-product').click(function() {
+		// Get root template template
+		var $template = $('.root-template').html();
+		var $element = '<div>' + $template + '</div>';
+		$('.additional').append($element);
+	});
+
+	$('.remove-template').live('click', function() {
+		var $divElement = $(this).parent().parent().parent().parent().parent();
+		if($divElement.hasClass('root-template')) {
+			alert('Root Template Can not be removed.');
+			return false;
+		} else {
+			$divElement.fadeOut('slow').remove();
+		}
+	});
 });
